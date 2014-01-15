b24670c180eea1cdcaca84e5ecbedb8bd2813d84
brett84c <brett84c@yahoo.com>
2014-01-15 09:47:05 -0500

    finished user signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -13,13 +13,6 @@ describe "User pages" do
   end
 
   describe "signup page" do
-    before { visit signup_path }
-
-    it { should have_content('Sign up') }
-    it { should have_title(full_title('Sign up')) }
-  end
-
-  describe "signup page" do
 
     before { visit signup_path }
 
